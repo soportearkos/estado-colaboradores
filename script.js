16e31784264fd52589bcda6629c9a7a3c3f5609d
soportearkos <soporte@arkos.com.co>
2024-07-05 08:09:05 -0500

    Cargue de archivos de la página web
    Se añade la carpeta Data_Base la cual contiene el csv correspondiente a la base de datos, se añade la carpeta img que contiene la imagen de fondo de la página, se añade el archivo html (index), css (styles) y js (script)

---
+++ script.js
@@ -0,0 +1,38 @@
+document.getElementById('numero_documentoForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const numero_documento = document.getElementById('numero_documento').value;
+    const resultDiv = document.getElementById('result');
+
+    Papa.parse('Data_Base/Base_Datos_Carnets.csv', {
+        download: true,
+        header: true,
+        complete: function (results) {
+            const data = results.data;
+            const empleado = data.find(emp => emp.NUMERO_DOCUMENTO === numero_documento);
+
+            if (empleado) {
+                resultDiv.style.display = 'block'; // Mostrar el cuadro de resultados
+                if (empleado.ESTADO.toLowerCase() === 'inactivo') {
+                    resultDiv.innerHTML = `
+                        <h2>Datos del Colaborador</h2>
+                        <p><span class="campos-label">Estado:</span> <span class="estado-valor-inactivo">${empleado.ESTADO}</span></p>
+                    `;
+                } else {
+                    resultDiv.innerHTML = `
+                        <h2>Datos del Colaborador</h2>
+                        <p><span class="campos-label">Estado:</span> <span class="estado-valor-activo">${empleado.ESTADO}</span></p>
+                        <p><span class="campos-label">Nombre completo:</span> ${empleado.NOMBRES_APELLIDOS}</p>
+                        <p><span class="campos-label">Cargo:</span> ${empleado.CARGO}</p>
+                        <p><span class="campos-label">Sede:</span> ${empleado.SEDE}</p>
+                        <p><span class="campos-label">ARL:</span> ${empleado.ARL}</p>
+                    `;
+                }
+            } else {
+                resultDiv.style.display = 'block'; // Mostrar el cuadro de resultados
+                resultDiv.innerHTML = `<p>No se encontraron datos para el documento ingresado.</p>`;
+            }
+        }
+    });
+});
+
